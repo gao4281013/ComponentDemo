3342080c871fe2a204e12efce1a4ac93a4fa5593
gavin <yongfeng.gao@pactera.com>
2018-05-31 13:45:48 +0800

    code gold module

--- ModuleGold/src/main/java/com/pactera/modulegold/di/module/GoldHomeModule.java
+++ ModuleGold/src/main/java/com/pactera/modulegold/di/module/GoldHomeModule.java
@@ -1,10 +1,26 @@
 package com.pactera.modulegold.di.module;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.jess.arms.di.scope.ActivityScope;
+import com.pactera.modulegold.mvp.contract.GoldHomeContract;
+import com.pactera.modulegold.mvp.model.GoldModel;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public abstract class GoldHomeModule {
 
+    @Binds
+    abstract GoldHomeContract.Model bindGoldModel(GoldModel goldModel);
 
+    @ActivityScope
+    @Provides
+    static RecyclerView.LayoutManager provideLayoutManager(GoldHomeContract.View view) {
+        return new LinearLayoutManager(view.getActivity());
+    }
 
 }
