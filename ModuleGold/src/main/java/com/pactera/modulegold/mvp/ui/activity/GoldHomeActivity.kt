b6b2cd165acb196d2bc53c5be87d683094ecde68
gavin <yongfeng.gao@pactera.com>
2018-06-10 22:01:19 +0800

    code gold module

---
+++ ModuleGold/src/main/java/com/pactera/modulegold/mvp/ui/activity/GoldHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.pactera.modulegold.mvp.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GoldHomeActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_gold_home)
+  }
+}
